a8769de9a8740694702fbc25da26be86d3452737
akshataa415-a11y <akshataa415@gmail.com>
2026-04-24 21:59:28 +0530

    Update Q06.py

--- Q06.py
+++ Q06.py
@@ -12,3 +12,10 @@
 # Sample Output 2:  0! = 1
 
 # --- YOUR CODE HERE ---
+n = int(input("Enter a number:"))
+#initialize result to 1
+for i in range (1,n+1):
+  factorial = factorial*i
+  print(f"{n}! = {factorial}")
+
+
